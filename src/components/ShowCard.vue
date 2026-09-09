8106f694178fe3a10b5494b3846dd48685f277d2
Shruthi Rangaswamy <shruthirangaswamy@peopleintouch.com>
2026-09-09 23:02:08 +0200

    feat: added show card and show genre components

---
+++ src/components/ShowCard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { Show } from '../types/show'
+import noImage from '../assets/no-image.png'
+
+defineProps<{
+  show: Show
+}>()
+</script>
+<template>
+  <article>
+    <img
+      v-if="show.image?.medium"
+      :src="show.image.medium"
+      :alt="show.name"
+    />
+    <div v-else>
+      <img :src="noImage" alt="No image available" />
+    </div>
+    <div class="show-card">
+      <h3>{{ show.name }}</h3>
+      <p>⭐ {{ show.rating.average ?? 'N/A' }}</p>
+    </div>
+  </article>
+</template>
+<style scoped>
+.show-card {
+  text-decoration: none;
+  color: inherit;
+}
+</style>
---
+++ src/components/ShowGenre.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import ShowCard from '../components/showCard.vue'
+import type { Show } from '../types/show.ts'
+
+defineProps<{
+  genre: string
+  shows: Show[]
+}>()
+</script>
+
+<template>
+  <section class="genre-section">
+    <h2>{{ genre }}</h2>
+
+    <div class="genre-section__shows">
+      <ShowCard
+        v-for="show in shows"
+        :key="show.id"
+        :show="show"
+      />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.genre-section {
+  margin-bottom: 32px;
+}
+
+.genre-section h2 {
+  margin-bottom: 16px;
+}
+
+.genre-section__shows {
+  display: flex;
+  gap: 16px;
+  overflow-x: auto;
+  padding-bottom: 8px;
+}
+</style>
